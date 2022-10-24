cf33f493281b7b55e4dca71f61d4819cab586e47
twknds <twknds@naver.com>
2022-10-24 16:56:57 +0900

    project basic commit

---
+++ src/test/java/com/opgg/opgg/OpggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opgg.opgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
